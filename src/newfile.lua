de4d250c75938c8d0d4f7fedf12dd040cf3eee36
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-07-13 18:47:41 +0500

    update src/newfile.lua

--- src/newfile.lua
+++ src/newfile.lua
@@ -27,16 +27,16 @@ function topbar:CreateTextWithIcon(TextWithIconConfig)
     
     if TextWithIconConfig.Icon ~= "" then
         local ImageLabel = Instance.new("ImageLabel", Frame)
-        ImageLabel.Image = TextWithIconConfig.Icon
+        ImageLabel.Image = "rbxassetid://" .. TextWithIconConfig.Icon
         ImageLabel.Size = UDim2.new(0,36,0,36)
-        ImageLabel.Position = UDim2.new(0,-52,0.5,0)
+        ImageLabel.Position = UDim2.new(0,-48,0.5,0)
         ImageLabel.AnchorPoint = Vector2.new(0,0.5)
         ImageLabel.BackgroundTransparency = 1
         
         local UICorner2 = Instance.new("UICorner", ImageLabel)
         UICorner2.CornerRadius = UDim.new(1,1)
         
-        UIPadding.PaddingLeft = UDim.new(0,56)
+        UIPadding.PaddingLeft = UDim.new(0,52)
     end
 end
 --[[
