fce5dc1ac7ecef0fe82b39531bba549e82899437
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-07-13 18:59:15 +0500

    update src/newfile.lua

--- src/newfile.lua
+++ src/newfile.lua
@@ -85,7 +85,7 @@ return topbar
 local TopbarApp = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlikSusFootages/RobloxTopBarApp/main/src/newfile.lua"))()
 
 TopbarApp:CreateTextWithIcon({
-    Icon = "rbxassetid://10747373176",
+    Icon = "user",
     Content = "example muhahaha",
     Order = 20
 })
