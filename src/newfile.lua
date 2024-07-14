4ebd0fca8c342fe67d3bea885df2eec4e7011704
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-07-14 12:06:30 +0500

    update src/newfile.lua

--- src/newfile.lua
+++ src/newfile.lua
@@ -37,11 +37,11 @@ end
 do
     local UnibarMenu = game.CoreGui.TopBarApp.UnibarLeftFrame.UnibarMenu
     
-    unibar.Position = UDim2.new(0, UnibarMenu.Size.X.Offset + 66, 0, 0)
+    unibar.Position = UDim2.new(0, 6, 0, 0)
     
-    UnibarMenu:GetPropertyChangedSignal("Size"):Connect(function()
-        unibar.Position = UDim2.new(0, UnibarMenu.Size.X.Offset + 66, 0, 0)
-    end)
+    -- UnibarMenu:GetPropertyChangedSignal("Size"):Connect(function()
+    --     unibar.Position = UDim2.new(0, UnibarMenu.Size.X.Offset + 66, 0, 0)
+    -- end)
 end
 
 function topbar:CreateTextWithIcon(TextWithIconConfig)
