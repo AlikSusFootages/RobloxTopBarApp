263c04caf93c514a6a7e9c602cbea99f4e811793
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-07-14 12:13:44 +0500

    update src/newfile.lua

--- src/newfile.lua
+++ src/newfile.lua
@@ -39,7 +39,7 @@ end
 do
     local UnibarMenu = game.CoreGui.TopBarApp.UnibarLeftFrame.UnibarMenu
     
-    unibar.Position = UDim2.new(0, UnibarMenu.Size.X.Offset + 12, 0, 0)
+    Parentframe.Position = UDim2.new(0, UnibarMenu.Size.X.Offset + 12, 0, 0)
     
     UnibarMenu:GetPropertyChangedSignal("Size"):Connect(function()
         unibar.Position = UDim2.new(0, UnibarMenu.Size.X.Offset + 12, 0, 0)
