42480524ab79b33d5110d627bd64d973cd8b14fc
.ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-08-04 21:02:55 +0500

    update src/file.lua

--- src/file.lua
+++ src/file.lua
@@ -192,9 +192,7 @@ if game.CoreGui.TopBar.TopBarFrame:FindFirstChild("UnibarLeftFrame") then else
         end
         
         
-        local anim = TweenService:Create(frame2, TweenInfo.new(0.5), {Position = UDim2.new(0, 0, 1, 0)}) 
-        
-        anim:Play() 
+        local anim = TweenService:Create(frame2, TweenInfo.new(0.5), {Position = UDim2.new(0, 0, 1, 0)}):Play() 
         
         local uicorner = Instance.new("UICorner", frame2) 
         uicorner.CornerRadius = UDim.new(0,8) 
@@ -258,7 +256,7 @@ if game.CoreGui.TopBar.TopBarFrame:FindFirstChild("UnibarLeftFrame") then else
         text.TextSize = 15 
         text.TextColor3 = Color3.fromRGB(255,255,255)
         
-        frame.Size = UDim2.new(0, text.TextBounds.X + 30, 1, 0)
+        frame.Size = UDim2.new(0, text.TextBounds.X + 38, 1, 0)
         
         if ToggleTextWithIconConfig.Icon == "" then
             
